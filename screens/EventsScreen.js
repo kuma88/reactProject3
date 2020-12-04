23e199ceef83688b78b79d7c1544537594501a12
kuma88 <kumarasond@gmail.com>
2020-12-04 11:05:46 +0800

    added stack screen

--- screens/EventsScreen.js
+++ screens/EventsScreen.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {createStackNavigator} from "@react-navigation/stack";
+
+function EventsHome(){
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Events!</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator()
 
 export default function EventsScreen() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Events!</Text>
-      </View>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="EventsHome"
+          component={EventsHome}
+        ></Stack.Screen>
+      </Stack.Navigator>
+
     );
   }
